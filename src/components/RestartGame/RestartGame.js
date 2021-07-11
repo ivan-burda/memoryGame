a9c6b859f3c53b85926453aa0bd922fe8c0e76be
ivanburda1986 <burda.ivan@gmail.com>
2021-07-11 18:01:57 +0200

    restart

---
+++ src/components/RestartGame/RestartGame.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { VscDebugRestart } from "react-icons/vsc";
+import { resetCards } from "../../actions/cards";
+import { resetFlipCount } from "../../actions/game";
+import imgFilenames from "../../media/imgFilenames";
+import { startTimer, resetTimer, unpauseTimer, pauseTimer } from "../../actions/timer";
+import classes from "./RestartGame.module.css";
+
+export default function RestartGame() {
+  const dispatch = useDispatch();
+  const pairs = useSelector((state) => state.game.pairs);
+
+  const restartGame = () => {
+    dispatch(resetCards(imgFilenames(pairs)));
+    setTimeout(() => {
+      dispatch(resetFlipCount());
+      dispatch(pauseTimer());
+      dispatch(resetTimer());
+    }, 500);
+    setTimeout(() => {
+      dispatch(startTimer());
+    }, 1000);
+  };
+
+  return (
+    <button type="button" className={classes.Button} onClick={() => restartGame()}>
+      Restart
+    </button>
+  );
+}
