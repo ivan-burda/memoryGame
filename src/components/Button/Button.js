519032f4378df6672c2710bb6593f21d8ed5ebba
ivanburda1986 <burda.ivan@gmail.com>
2021-07-14 00:18:27 +0200

    removed disabled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import classes from "./Button.module.css";
 
 export default function Button(props) {
   return (
-    <button type="button" onClick={props.action} className={classes.Button} disabled>
+    <button type="button" onClick={props.action} className={classes.Button}>
       {props.title}
     </button>
   );
